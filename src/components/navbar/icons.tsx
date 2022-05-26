dea4f5f3118329070cdbd35c2bd23d1d2686ff55
ntabucejo <ntabucejo@gmail.com>
2022-05-27 03:13:18 +0800

    Fix prop name

--- src/components/navbar/icons.tsx
+++ src/components/navbar/icons.tsx
@@ -5,11 +5,11 @@ export const MessengerIcon = () => {
         d="M12.003 2.001a9.705 9.705 0 110 19.4 10.876 10.876 0 01-2.895-.384.798.798 0 00-.533.04l-1.984.876a.801.801 0 01-1.123-.708l-.054-1.78a.806.806 0 00-.27-.569 9.49 9.49 0 01-3.14-7.175 9.65 9.65 0 0110-9.7z"
         fill="none"
         stroke="currentColor"
-        stroke-miterlimit="10"
-        stroke-width="1.739"></path>
+        strokeMiterlimit="10"
+        strokeWidth="1.739"></path>
       <path
         d="M17.79 10.132a.659.659 0 00-.962-.873l-2.556 2.05a.63.63 0 01-.758.002L11.06 9.47a1.576 1.576 0 00-2.277.42l-2.567 3.98a.659.659 0 00.961.875l2.556-2.049a.63.63 0 01.759-.002l2.452 1.84a1.576 1.576 0 002.278-.42z"
-        fill-rule="evenodd"></path>
+        fillRule="evenodd"></path>
     </svg>
   )
 }
@@ -21,15 +21,15 @@ export const PostIcon = () => {
         d="M2 12v3.45c0 2.849.698 4.005 1.606 4.944.94.909 2.098 1.608 4.946 1.608h6.896c2.848 0 4.006-.7 4.946-1.608C21.302 19.455 22 18.3 22 15.45V8.552c0-2.849-.698-4.006-1.606-4.945C19.454 2.7 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.547 2 5.703 2 8.552z"
         fill="none"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"></path>
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"></path>
       <line
         fill="none"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         x1="6.545"
         x2="17.455"
         y1="12.001"
@@ -37,9 +37,9 @@ export const PostIcon = () => {
       <line
         fill="none"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         x1="12.003"
         x2="12.003"
         y1="6.545"
@@ -55,19 +55,19 @@ export const ExploreIcon = () => {
         fill="none"
         points="13.941 13.953 7.581 16.424 10.06 10.056 16.42 7.585 13.941 13.953"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"></polygon>
-      <polygon fill-rule="evenodd" points="10.06 10.056 13.949 13.945 7.581 16.424 10.06 10.056"></polygon>
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"></polygon>
+      <polygon fillRule="evenodd" points="10.06 10.056 13.949 13.945 7.581 16.424 10.06 10.056"></polygon>
       <circle
         cx="12.001"
         cy="12.005"
         fill="none"
         r="10.5"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"></circle>
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"></circle>
     </svg>
   )
 }
