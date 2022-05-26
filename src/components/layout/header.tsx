3ff89f39ac314786e2f38ced0df066923d3fbcdf
ntabucejo <ntabucejo@gmail.com>
2022-05-27 02:30:44 +0800

    Replace shadow to border

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,7 +2,7 @@ import Navbar from '../navbar'
 
 const Header = () => {
   return (
-    <header className="bg-white shadow-lg">
+    <header className="bg-white border-2 border-gray-300">
       <div className="max-w-5xl mx-auto">
         <Navbar />
       </div>
