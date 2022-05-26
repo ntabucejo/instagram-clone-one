1853d06ed4ba090ffe9557d9a10dc568c81ffc2d
ntabucejo <ntabucejo@gmail.com>
2022-05-27 03:08:00 +0800

    Add feed

---
+++ src/components/feed/index.tsx
@@ -0,0 +1,11 @@
+import Post from './post'
+
+const Feed = () => {
+  return (
+    <section>
+      <Post />
+    </section>
+  )
+}
+
+export default Feed
---
+++ src/components/feed/post.tsx
@@ -0,0 +1,5 @@
+const Post = () => {
+  return <div className="container">Post</div>
+}
+
+export default Post
