a7f3e35418722c227c8ba6fe34af3d8b6200f399
ntabucejo <ntabucejo@gmail.com>
2022-05-27 15:40:31 +0800

    Fix searchbar color

--- src/components/navbar/searchbar.tsx
+++ src/components/navbar/searchbar.tsx
@@ -2,8 +2,8 @@ import { SearchIcon } from '@heroicons/react/outline'
 
 const Searchbar = () => {
   return (
-    <div className="bg-neutral-200 px-4 py-1.5 rounded-lg md:flex items-center gap-2 text-gray-400 hidden">
-      <SearchIcon className="w-5 h-5" />
+    <div className="bg-neutral-200 px-4 py-1.5 rounded-lg md:flex items-center gap-2 hidden">
+      <SearchIcon className="w-5 h-5 text-gray-400" />
       <input type="text" placeholder="Search" className="bg-transparent focus:outline-none" />
     </div>
   )
