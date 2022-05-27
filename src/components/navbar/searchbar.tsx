db9cfc926fedf7205d068010db85d33c09e53383
ntabucejo <ntabucejo@gmail.com>
2022-05-27 15:44:17 +0800

    Update navbar breakpoint

--- src/components/navbar/searchbar.tsx
+++ src/components/navbar/searchbar.tsx
@@ -2,7 +2,7 @@ import { SearchIcon } from '@heroicons/react/outline'
 
 const Searchbar = () => {
   return (
-    <div className="bg-neutral-200 px-4 py-1.5 rounded-lg md:flex items-center gap-2 hidden">
+    <div className="bg-neutral-200 px-4 py-1.5 rounded-lg flex items-center gap-2 ">
       <SearchIcon className="w-5 h-5 text-gray-400" />
       <input type="text" placeholder="Search" className="bg-transparent focus:outline-none" />
     </div>
