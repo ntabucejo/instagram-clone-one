cced07ac00813dda44cbc1b3a567961763a99078
ntabucejo <ntabucejo@gmail.com>
2022-05-27 14:49:00 +0800

    Add breakpoint to hide links

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -4,20 +4,22 @@ import { HomeIcon } from '@heroicons/react/solid'
 import Path from './path'
 import { ExploreIcon, HeartIcon, MessengerIcon, PostIcon } from './icons'
 import Profile from './profile'
+import { MenuAlt4Icon } from '@heroicons/react/outline'
 
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between px-5 h-14 gap-5">
       <Logo />
       <Searchbar />
-      <div className='flex items-center gap-5'>
-        <ul className="flex items-center gap-5">
+      <div className="flex items-center gap-5">
+        <ul className="sm:flex items-center gap-5 hidden">
           <Path Icon={HomeIcon} />
           <Path Icon={MessengerIcon} />
           <Path Icon={PostIcon} />
           <Path Icon={ExploreIcon} />
           <Path Icon={HeartIcon} />
         </ul>
+        <MenuAlt4Icon className="w-6 sm:hidden" />
         <Profile />
       </div>
     </nav>
